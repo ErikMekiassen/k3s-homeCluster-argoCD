d9555c7c1dbda63fd1b8260a2964ba46e6dd7174
Erik Mekiassen <erik.mekiassen@spk.no>
2024-04-17 14:41:22 +0200

    fjerner prompts

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,7 @@
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -q -N ""
 
 # Add Docker's official GPG key:
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -12,8 +12,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo docker run hello-world
 
 wget -NP . https://dokku.com/install/v0.34.4/bootstrap.sh
