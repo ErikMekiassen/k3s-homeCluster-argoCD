095ddaf1b15b016a6717a5561f4d1102d5b04a32
Erik Mekiassen <erik.mekiassen@spk.no>
2024-04-17 14:33:22 +0200

    la til dokku installation

---
+++ scripts/docker.sh
@@ -0,0 +1,23 @@
+ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -q -N ""
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+
+wget -NP . https://dokku.com/install/v0.34.4/bootstrap.sh
+sudo DOKKU_TAG=v0.34.4 bash bootstrap.sh
+cat ~/.ssh/authorized_keys | dokku ssh-keys:add admin
+dokku domains:set-global mekiassen.com # endre!
+dokku domains:set-global 192.168.1.111.sslip.io
